760b215c8987abb15b460c5fac0c065bad89e81d
paulus <paulusojak@gmail.com>
2024-04-07 02:05:01 +0700

    Add a working reset button to change grid size

--- scripts.js
+++ scripts.js
@@ -9,25 +9,27 @@ container.style.width = '720px';
 container.style.height = '720px';
 container.style.flexWrap = 'wrap';
 
+let n = 1;
+const gridElements = [];
+
+
 
 function createGrid(size){
     let gridSize = 720 / size;
     for (let i = 0; i < size; i++){
+        gridElements[i] = [];
         for (let j = 0; j < size; j++){
             const grid = document.createElement('div');
-            grid.className = 'grid';
-            //grid.style.backgroundColor = 'black';
+            grid.className = `grid${n}`;
             grid.style.outline = "1px solid black";
-            //grid.textContent = x;
-            //grid.style.display = 'flex';
-            //grid.style.flexDirection = 'column';
             grid.style.flexBasis = gridSize + 'px';
-
             container.appendChild(grid);
+            gridElements[i][j] = grid;
         }
     }
-
-    const grid = document.querySelectorAll('.grid');
+    console.log('row :' + gridElements.length);
+    console.log('column :' + gridElements[0].length);
+    const grid = document.querySelectorAll(`.grid${n}`);
     console.log(grid.length);
 
     //Loop to detect all grid events
@@ -36,7 +38,13 @@ function createGrid(size){
             grid[i].style.backgroundColor = 'black';
         });
     }
+    return gridElements;
 }
+
+createGrid(50);
+
+console.log('gridElements.length' + gridElements.length);
+console.log('gridElements[0].length' + gridElements[0].length);
 
 const boxSize = document.createElement('div');
 main.insertBefore(boxSize, main.firstChild);
@@ -55,7 +63,15 @@ boxSize.appendChild(boxSizeButton);
 boxSizeButton.addEventListener('click', (e) => {
     if (boxSizeInput.value >= 1 && 
         boxSizeInput.value <= 100){
+            for(let i = 0; i < gridElements.length; i++){
+                for(let j = 0; j < gridElements[i].length; j++){
+                    gridElements[i][j].remove();
+                }
+            }
             createGrid(boxSizeInput.value);
         }
+    else {
+        alert('Grid size error! Please check your value!');
+    }
 });
 
